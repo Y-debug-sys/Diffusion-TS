f004bc065a4c700518447306963993d88cbc17cf
Xinyu Yuan <yxy5315@gmail.com>
2024-04-18 16:02:41 +0800

    Update

--- Models/interpretable_diffusion/gaussian_diffusion.py
+++ Models/interpretable_diffusion/gaussian_diffusion.py
@@ -337,14 +337,13 @@ class Diffusion_TS(nn.Module):
         target,
         t: int,
         partial_mask=None,
-        x_self_cond=None,
         clip_denoised=True,
         model_kwargs=None
     ):
         b, *_, device = *x.shape, self.betas.device
         batched_times = torch.full((x.shape[0],), t, device=x.device, dtype=torch.long)
         model_mean, _, model_log_variance, _ = \
-            self.p_mean_variance(x=x, t=batched_times, x_self_cond=x_self_cond, clip_denoised=clip_denoised)
+            self.p_mean_variance(x=x, t=batched_times, clip_denoised=clip_denoised)
         noise = torch.randn_like(x) if t > 0 else 0.  # no noise if t == 0
         sigma = (0.5 * model_log_variance).exp()
         pred_img = model_mean + sigma * noise

Fix Data Leakage

Utils/Data_utils/real_datasets.py
@@ -114,8 +114,8 @@ class CustomDataset(Dataset):
         np.random.seed(seed)
 
         regular_train_num = int(np.ceil(size * ratio))
-        id_rdm = np.random.permutation(size)
-        # id_rdm = np.arange(size)
+        # id_rdm = np.random.permutation(size)
+        id_rdm = np.arange(size)
         regular_train_id = id_rdm[:regular_train_num]
         irregular_train_id = id_rdm[regular_train_num:]
 